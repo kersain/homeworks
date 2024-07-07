0a73486c915fc5111cca0bcb5ab1623212426ebd
kliv <kersainvas@gmail.com>
2024-07-07 12:45:55 +0300

    Commit - homework2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,14 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: {
+    street: string
+    city: string
+  }; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -44,11 +47,12 @@ export const HW2 = () => {
     ]
   }
 
-  let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
+  let [currentUsers, setCurrentUsers] = useState(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter((fUsers) =>fUsers.address.city === 'Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
+
   };
 
   return (
